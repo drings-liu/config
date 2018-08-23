0a9cc8f47e150a28055d3166d86ca0a3bfb1dbbc
kevin.liu <kevin.liu@idreamsky.com>
2018-08-23 17:34:38 +0800

    修改配置

--- db_config.php
+++ db_config.php
@@ -1,7 +1,7 @@
 <?php
 
 $db_config=array(
-	'db_user_host'=>'192.168.1.215',
+	'db_user_host'=>'172.168.1.215',
 	'db_user_name'=>'demo_user1',
-	'db_user_pswd'=>'123456111'
+	'db_user_pswd'=>'abcdef001'
 );
